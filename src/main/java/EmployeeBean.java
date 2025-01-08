4c9e04e99dd5918d3d8a452339835501c97e31c3
Suraj Shetty <59719375+surajshetty09@users.noreply.github.com>
2025-01-08 10:14:51 +0530

    Update EmployeeBean.java

--- src/main/java/EmployeeBean.java
+++ src/main/java/EmployeeBean.java
@@ -60,7 +60,7 @@ public class EmployeeBean implements Serializable {
         }
     	
     	
-    	System.out.println("In init() before calling : goToModifyEmployeeFetch");
+    	System.out.println("In init() before calling : goToModifyEmployeeFetch 1");
     	this.selectedEmployee = new Employee();
     	System.out.println("init() flag:"+modFlag);
     	if(modFlag) {
